6bc5be2a5e736274065f2f675ac2e8ad7417d354
Thomas Lemoine <contact@thomaslemoine.net>
2021-05-11 15:03:02 +0200

    refactor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let csvToJson = require('convert-csv-to-json');
 
 let fileInputName = 'Data_header.csv';
-let fileOutputName = 'myOutputFile.json';
+let fileOutputName = 'groupe8.json';
 
 csvToJson.generateJsonFileFromCsv(fileInputName, fileOutputName);
